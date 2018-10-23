eee6c14551c34ef9e6178d69bce784a294978bb0
rahee <ra256@njit.edu>
2018-10-23 18:16:20 -0400

    Task: Fixed Contact Form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,41 +2,27 @@
 
 @section('content')
 
-
-
-    <h1>Contact</h1>
-    <p class="lead">Please use this form to contact the site owner.</p>
-
-    <form>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <div class="card mt - 3 pl-2 pr-2">
+        <div class="card-title"><h1><b>Welcome to our Contact Page.</b></h1>
+            <p class="lead">Please use the form and phone number on this site to contact the site owner for
+                sponsorship <br> or if you have any questions about laravel</p></div>
+        <div class="card-body">
+            <form role="form" id="contact-form" class="contact-form" method="POST" action="{{route('contact.store')}}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="name">Your Name</label>
+                    <input name="name" type="text" class="form-control" id="name" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="yourname@domain.com">
+                </div>
+                <div class="form-group">
+                    <label for="body">Please enter your message below</label>
+                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select multiple class="form-control" id="exampleFormControlSelect2">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-    </form>
-
-
+    </div>
 @endsection
