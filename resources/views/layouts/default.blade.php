6a88947c18f6e5336de932ef3950fc1ccd968047
rahee <ra256@njit.edu>
2018-10-22 21:34:10 -0400

    Task: Added JumboTron

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,10 +4,14 @@
 @include('includes.head')
 
 <body>
+    @include('includes.menu')
 
-
-    @yield('content')
+    <main role="main"></main>
+        @yield('content')
+    </main>
 
 @include('includes.jsfooter')
+@include('includes.footer')
+
 </body>
 </html>
